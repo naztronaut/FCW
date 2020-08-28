6e6ef2e539ae7e300c40426c54253a1f5d86cc57
Nazmus Nasir <nazmus.nasir@gmail.com>
2020-08-28 00:00:46 -0400

    moved neopixel stuff to neopix.py so it can be run with sudo

--- led_service.py
+++ led_service.py
@@ -1,12 +1,15 @@
-import board
-import neopixel
-import config
+import os
+#
+# import board
+# import neopixel
+# import config
 
 # Change to the number of LEDs you are using
 # Uses GPIO Pin 18. if you want to use another pin, change the D18 to some other pin
-pixels = neopixel.NeoPixel(board.D18, config.LED_COUNT)
+# pixels = neopixel.NeoPixel(board.D18, config.LED_COUNT)
 
 
 def update_led(red, green, blue):
-    for x in range(0, config.LED_COUNT):
-        pixels[x] = (red, green, blue)
+    os.popen('sudo python3 ' + os.path.dirname(os.path.realpath(__file__)) + '/neopix.py  ' + red + ' ' + green + ' ' + blue)
+    # for x in range(0, config.LED_COUNT):
+    #     pixels[x] = (red, green, blue)
---
+++ neopix.py
@@ -0,0 +1,14 @@
+import sys
+import board
+import neopixel
+import config
+
+# Uses GPIO Pin 18. if you want to use another pin, change the D18 to some other pin
+pixels = neopixel.NeoPixel(board.D18, config.LED_COUNT)
+
+red = sys.argv[1]
+green = sys.argv[2]
+blue = sys.argv[3]
+
+for x in range(0, config.LED_COUNT):
+    pixels[x] = (red, green, blue)
