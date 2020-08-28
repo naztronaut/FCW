abd8a2ce7255cd6d490021a263cff5ff3f9e85ca
Nazmus Nasir <nazmus.nasir@gmail.com>
2020-08-27 23:54:18 -0400

    reset flask controller

--- led.py
+++ led.py
@@ -4,14 +4,13 @@ import led_service as ls
 app = Flask(__name__)
 
 
-# {{url}}/led?type={{simple/neopixel}}&status=on/off
+# {{url}}/led?red=255&green=255&blue=255&position=1
 @app.route('/', methods=['GET'])
 def led():
-    led_type = request.args.get('type')
-    status = request.args.get('status')
-    if 'theme' in request.args:
-        theme = request.args.get('theme')
-    else:
-        theme = 'random'
-    resp = ls.update_led(led_type, status, theme)
+    red = request.args.get('red')
+    green = request.args.get('green')
+    blue = request.args.get('blue')
+
+    resp = ls.update_led(red, green, blue)
     return jsonify({"message": resp})
+
